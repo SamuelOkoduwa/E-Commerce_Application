7001924ec90890a838832a8d760a0d9eb0811e9c
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-06-08 12:56:06 +0100

    applied the importation of neccessary dependencies

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
-
+require('dotenv').config();
+const connectDB = require('./config/db');	// Import connectDB function
+const morgan = require('morgan');	// Import morgan
 
 const app = express();
+
+// Connect to database
+connectDB();	// Call connectDB function
+
+// Middleware
+app.use(express.json());	// Body parser
+app.use(morgan('tiny'));	// Morgan
+
+// API routes
+app.get('/api', (req, res) => {
+	res.send('Hello World');
+});
 
 // Server static assets if in production
 const PORT = process.env.PORT || 3000;
