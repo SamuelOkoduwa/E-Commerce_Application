11edbbf7489691fb9135139bf62c3886d29ca124
Samuel Okoduwa <samuel_okoduwa@yahoo.com>
2024-06-08 16:48:50 +0100

    modifing using template literals' on the try block

--- config/db.js
+++ config/db.js
@@ -3,10 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
 	try {
-		await mongoose.connect(process.env.MONGO_URI, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
+		await mongoose.connect(`${process.env.MONGO_URI}`);
 		console.log('Database connected ...');
 	} catch (error) {
 		console.error(error.message);
