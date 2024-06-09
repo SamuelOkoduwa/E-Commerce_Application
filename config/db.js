de62f7a6048cd8727fc18cac1dcec5473762fd8a
princeodike90 <princeodike90@gmail.com>
2024-06-09 20:26:33 +0100

    modified MONGODB_URL

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
 	try {
-		await mongoose.connect(`${process.env.MONGO_URI}`);
+		await mongoose.connect(`${process.env.MONGODB_URL}`);
 		console.log('Database connected ...');
 	} catch (error) {
 		console.error(error.message);
